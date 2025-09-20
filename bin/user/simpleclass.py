8248dbda8dacb4a3adfa84a75ac8b4b4d124376e
Rich Bell <bellrichm@gmail.com>
2025-09-20 13:30:15 -0400

    add some code for testing the workflows

---
+++ bin/user/simpleclass.py
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+#    Copyright (c) 2025 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+''' This module is used to test the development tools.'''
+
+
+class SimpleClass:
+    ''' This class is used to test the development tools.'''
+    def return_first_value(self, first_parameter, _second_parameter):
+        ''' Return the method's first parameter.'''
+        return first_parameter
+
+    def return_second_value(self, _first_parameter, second_parameter):
+        ''' Return the method's second parameter.'''
+        return second_parameter
