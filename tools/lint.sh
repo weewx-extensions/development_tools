88bd993f03f7a9fdbb9b52cc88de8ee393287983
Rich Bell <bellrichm@gmail.com>
2025-12-03 11:54:15 -0500

    update pylint dev tool invocation

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,1 +1,11 @@
-pylint bin/user/$WEEWX_MODULE_NAME
+#! /bin/bash
+#
+#    Copyright (c) 2025 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+
+export PYENV_VERSION=$WEEWX_PYENV_VERSION
+export PYTHONPATH=$WEEWX_PYTHONPATH
+
+pylint bin/user/
